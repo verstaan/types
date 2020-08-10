7cdcb93b331ad37148bde43812cd073268821f5a
Rahul Deo Joshi <career.rdj@gmail.com>
2020-08-10 17:42:47 -0400

    Updated analytics Summary (#7)
    * Updated analytics Summary
    
    * Updated analytics

--- analytics/analyticsResponse.ts
+++ analytics/analyticsResponse.ts
@@ -100,38 +100,15 @@ export class Analytics {
 /**
  * Sherlock V2 analytics summary
  */
-export class GeneralStats {
-    public count: number | undefined;
-    public change: number | undefined;
-    public day: string | undefined;
-    public month: string | undefined;
-    public date: string | undefined;
-
-    constructor(init?: Partial<GeneralStats>) {
-        Object.assign(this, init);
-    }
-
-    checkInfo(): boolean {
-        return (
-            this.count != undefined &&
-            this.change != undefined &&
-            this.day != undefined &&
-            this.month != undefined &&
-            this.date != undefined
-        )
-    }
-}
-
-
 export class PerCategoryStats {
     public category: string | undefined;
     public count: number | undefined;
     public change: number | undefined;
     public day: string | undefined;
     public month: string | undefined;
-    public date: string | undefined;
+    public dateTime: string | undefined;
 
-    constructor(init?: Partial<GeneralStats>) {
+    constructor(init?: Partial<PerCategoryStats>) {
         Object.assign(this, init);
     }
 
@@ -142,14 +119,39 @@ export class PerCategoryStats {
             this.change != undefined &&
             this.day != undefined &&
             this.month != undefined &&
-            this.date != undefined
+            this.dateTime != undefined
         )
     }
 }
 
+
+export class CategoryStats {
+    public category: string | undefined;
+    public count: number | undefined;
+    public change: number | undefined;
+
+    constructor(init?: Partial<CategoryStats>) {
+        Object.assign(this, init);
+    }
+
+    checkInfo(): boolean {
+        return (
+            this.category != undefined &&
+            this.count != undefined &&
+            this.change != undefined
+        )
+    }
+}
+
+
 export class AnalyticsSummary {
-    public generalStats: GeneralStats[] | undefined;
     public perCategoryStats: PerCategoryStats[] | undefined;
+    public pastDayHourlyCategoryStats: PerCategoryStats[] | undefined;
+    public allTimeCategoryStatsPerWeek: PerCategoryStats[] | undefined;
+    public pastDayCategoryStats: CategoryStats[] | undefined;
+    public pastWeekCategoryStats: CategoryStats[] | undefined;
+    public pastMonthCategoryStats: CategoryStats[] | undefined;
+    public allTimeCategoryStats: CategoryStats[] | undefined;
 
     constructor(init?: Partial<AnalyticsSummary>) {
         Object.assign(this, init);
@@ -157,8 +159,11 @@ export class AnalyticsSummary {
 
     checkInfo(): boolean {
         return (
-            this.generalStats != undefined &&
-            this.perCategoryStats != undefined
+            this.perCategoryStats != undefined &&
+            this.pastDayCategoryStats != undefined &&
+            this.pastWeekCategoryStats != undefined &&
+            this.pastMonthCategoryStats != undefined &&
+            this.allTimeCategoryStats != undefined
         )
     }
 }
