bf25f9e6e24830e78d36726a4a99c611481597e9
Michael <mcipolla@verstaan.com>
2020-08-14 15:39:22 -0400

    included new userprofile fields in newUser class

--- newUser.ts
+++ newUser.ts
@@ -4,8 +4,20 @@ export class NewUser {
   public lastname: string | undefined;
   public password: string | undefined;
   public client_id: number | undefined;
+  public team_id?: number | null | undefined;
   public role: number | undefined;
-  public designation: string | undefined;
+  public designation: string | null | undefined;
+  public nickname?: string | undefined;
+  public position?: string | undefined;
+  public address_home?: string | undefined;
+  public phone_cell?: string | undefined;
+  public phone_work?: string | undefined;
+  public phone_home?: string | undefined;
+  public emergency_contact?: string | undefined;
+  public emergency_contact_relationship?: string | undefined;
+  public emergency_phone_home?: string | undefined;
+  public emergency_phone_cell?: string | undefined;
+  public emergency_phone_work?: string | undefined;
 
   constructor(init?: Partial<NewUser>) {
     Object.assign(this, init);
@@ -18,10 +30,10 @@ export class NewUser {
    */
   checkInfo(): boolean {
     return (
-      this.email != undefined &&
-      this.firstname != undefined &&
-      this.lastname != undefined &&
-      this.client_id != undefined
+      this.email !== undefined &&
+      this.firstname !== undefined &&
+      this.lastname !== undefined &&
+      this.client_id !== undefined
     );
   }
 }
