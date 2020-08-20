33b980058d0c77a284a18651644c658824c67447
NicoBowden <nbowden21@groton.org>
2020-08-20 18:21:35 -0400

    UserUpdate team_id fix for back end

--- index.ts
+++ index.ts
@@ -1347,7 +1347,7 @@ export interface TeamUpdate {
 
 export interface UserUpdate {
     id: number,
-    team_id?: number,
+    team_id?: number | null,
     role?: number,
     designation?: string
 }
