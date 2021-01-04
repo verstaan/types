398514ab510719ee6283b1c265f6e217f8acab17
Rahul Joshi <career.rdj@gmail.com>
2021-01-04 11:11:25 -0500

    Updated login for new JWT auth

--- login.ts
+++ login.ts
@@ -1,11 +1,13 @@
 export class Login {
-  public iat: string;
-  public exp: string;
+  // public iat: string;
+  // public exp: string;
   public token: string;
+  public refreshToken: string;
 
-  constructor(iat: string, exp: string, token: string) {
-    this.iat = iat;
-    this.exp = exp;
+  constructor(token: string, refreshToken: string) {
+    // this.iat = iat;
+    // this.exp = exp;
     this.token = token;
+    this.refreshToken = refreshToken;
   }
 }
