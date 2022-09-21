12dcc4166a5946642c045bdb1b019fcd27b7ef6c
Joseph Bernardi <joe@josephrbernardi.com>
2022-09-21 12:41:01 -0400

    trip, destination, and travel user additions

--- travelApp.ts
+++ travelApp.ts
@@ -1,7 +1,17 @@
-import { MultiPolygon, Polygon } from "geojson";
+import { MultiPolygon, Polygon, Point } from "geojson";
 
 export interface TripCharacteristics {
     has_children: boolean | null;
+}
+
+export interface UserCharacteristics {
+    sex: string;
+    socioecon: string;
+}
+
+export interface DestinationCharacteristics {
+    crowded_factors: string[];
+    time_in_public: number // out of 10, 10 being the most time in public
 }
 
 export interface TravelTrip {
@@ -15,3 +25,20 @@ export interface TravelTrip {
     trip_owner: number;
 }
 
+export interface TravelUser {
+    email: string;
+    date_joined: string;
+    first_name: string;
+    last_name: string;
+    user_characteristics: UserCharacteristics;
+    is_pending: boolean;
+}
+
+export interface TravelDestination {
+    name: string;
+    trip_id: number;
+    group_id: number | null;
+    point_location: Point;
+    destination_characteristics: DestinationCharacteristics;
+    destination_type: string | null;
+}
